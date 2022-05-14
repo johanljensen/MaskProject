69c0364348b21f6243a0e37121074cd5b66df3a4
Johan L Jensen <johanljensen@hotmail.com>
2022-05-14 22:07:54 +0200

    Storing edit data when switching images

---
+++ MaskProject/LoadedImage.py
@@ -0,0 +1,16 @@
+
+
+class LoadedImage:
+
+    def __init__(self):
+
+        self.imageName = ""
+        self.baseImagePath = ""
+        self.editImagePath = ""
+        self.baseGraphic = None
+        self.currentGraphic = None
+
+        self.maskList = []
+        self.classList = []
+        self.groupList = []
+        self.selectedMask = None
--- MaskProject/MaskManager.py
+++ MaskProject/MaskManager.py
@@ -3,85 +3,113 @@ import numpy as np
 import timeit
 from PyQt5.QtGui import QPixmap
 
+from LoadedImage import LoadedImage
 from MaskSelection import MaskSelection
 from SimpleEdits import SimpleEdits
 from CurveTool import CurveTool
 from Blur import Blur
+
 
 class MaskManager:
 
     def __init__(self):
         print("MaskManager made")
 
-        self.imagePath = 'imageData/Royal Guard/'
+        self.imageBasePath = 'imageData/'
+        self.inputFilename = '/input.jpg'
+        self.editFilename = '/p6_mask_edit.png'
 
-        self.baseImage = cv2.imread(self.imagePath + 'input.jpg')
-        self.currentImage = self.baseImage.copy()
+        self.baseGraphic = None
 
-        self.displayGraphic = None
+        self.displayGraphicLabel = None
         self.instanceDropdown = None
         self.classDropdown = None
-
+        self.groupDropdown = None
 
         self.simpleEdits = SimpleEdits()
         self.curveTool = CurveTool()
         self.blur = Blur()
 
-        self.maskList, self.classList = MaskSelection().DetectMasks(self.imagePath)
-        self.groupList = []
-
-        self.selectedMask = None
+        self.loadedImages = []
+        self.selectedImage = None
         self.selectedIsGroup = False
 
         self.drawTimer = timeit.default_timer()
 
     def SetUIreferences(self, imageLabel, instanceDropdown, classDropDown):
-        self.displayGraphic = imageLabel
+        self.displayGraphicLabel = imageLabel
         self.instanceDropdown = instanceDropdown
         self.classDropdown = classDropDown
 
     def SetMaskLists(self):
-        self.maskList, self.classList = MaskSelection().DetectMasks(self.imagePath)
 
         self.instanceDropdown.clear()
-        for mask in self.maskList:
+        for mask in self.selectedImage.maskList:
             self.instanceDropdown.addItem(mask.maskName)
 
         self.classDropdown.clear()
-        for maskClass in self.classList:
+        for maskClass in self.selectedImage.classList:
             self.classDropdown.addItem(maskClass.groupName)
 
-        self.selectedMask = self.maskList[0]
+        self.selectedMask = self.selectedImage.maskList[0]
+
+    def GetImageMasks(self, loadedImage):
+        loadedImage.maskList, loadedImage.classList = MaskSelection().DetectMasks(self.imageFullPath)
 
     def ImageSelect(self, imageName):
         print("Switched to image: " + imageName)
-        self.imagePath = "imageData/" + imageName + "/"
-        self.displayGraphic.setPixmap(QPixmap(self.imagePath + "input.jpg"))
-        self.baseImage = cv2.imread(self.imagePath + 'input.jpg')
-        self.currentImage = self.baseImage.copy()
+        self.imageFullPath = self.imageBasePath + imageName + "/"
+        self.displayGraphicLabel.setPixmap(QPixmap(self.imageFullPath + self.inputFilename))
+        self.baseGraphic = cv2.imread(self.imageFullPath + self.inputFilename)
+
+        newImage = None
+        for image in self.loadedImages:
+            print(image.imageName + " --- " + imageName)
+            if image.imageName == imageName:
+                newImage = image
+                break
+
+        if newImage is None:
+            newImage = self.CreateMaskImage(imageName)
+
+        self.selectedImage = newImage
+        self.displayGraphicLabel.setPixmap(QPixmap(self.imageFullPath + self.editFilename))
         self.SetMaskLists()
+
+    def CreateMaskImage(self, imageName):
+        newImage = LoadedImage()
+        newImage.imageName = imageName
+        newImage.baseImagePath = self.imageBasePath + imageName + self.inputFilename
+        newImage.editImagePath = self.imageBasePath + imageName + self.editFilename
+        newImage.baseGraphic = cv2.imread(self.imageBasePath + imageName + self.inputFilename)
+        newImage.currentGraphic = newImage.baseGraphic.copy()
+        cv2.imwrite(newImage.editImagePath, newImage.currentGraphic)
+
+        self.loadedImages.append(newImage)
+        self.GetImageMasks(newImage)
+
+        return newImage
 
     def InstanceSelect(self, index):
         print("Selected instance: " + str(index))
-        self.selectedMask = self.maskList[index]
+        self.selectedMask = self.selectedImage.maskList[index]
         self.selectedIsGroup = False
 
     def ClassSelect(self, index):
         print("Selected class: " + str(index))
-        self.selectedMask = self.classList[index]
+        self.selectedMask = self.selectedImage.classList[index]
         self.selectedIsGroup = True
 
     def DrawCurrentImage(self):
-
-        drawingImg = self.baseImage.copy()
-        for mask in self.maskList:
+        drawingImg = self.baseGraphic.copy()
+        for mask in self.selectedImage.maskList:
 
             groupSettings = []
-            for cMask in self.classList:
+            for cMask in self.selectedImage.classList:
                 if cMask.maskList.__contains__(mask):
                     groupSettings.append(cMask.maskSettings)
 
-            for gMask in self.groupList:
+            for gMask in self.selectedImage.groupList:
                 if gMask.maskList.__contains__(mask):
                     groupSettings.append(gMask.maskSettings)
 
@@ -95,23 +123,23 @@ class MaskManager:
             drawingImg = self.simpleEdits.DrawBrightness(totalBrightness, drawingImg)
             drawingImg = self.simpleEdits.DrawSaturation(totalSaturation, drawingImg)
 
-            drawingImg = np.where(self.selectedMask.maskTrueFalse == True, drawingImg, self.currentImage)
+            drawingImg = np.where(self.selectedMask.maskTrueFalse == True, drawingImg, self.selectedImage.currentGraphic)
 
-        cv2.imwrite(self.imagePath + 'p6_maskedit.png', drawingImg)
-        pixmap = QPixmap(self.imagePath + 'p6_maskedit.png')
-        self.displayGraphic.setPixmap(pixmap)
-        self.currentImage = drawingImg
+        cv2.imwrite(self.imageFullPath + self.editFilename, drawingImg)
+        pixmap = QPixmap(self.imageFullPath + self.editFilename)
+        self.displayGraphicLabel.setPixmap(pixmap)
+        self.selectedImage.currentGraphic = drawingImg
 
     # By only operating on the bounding box of the mask, this version gains a little speed
     def DrawMask(self, mask):
-        drawingImg = self.baseImage.copy()[mask.minX:mask.maxX, mask.minY:mask.maxY]
+        drawingImg = self.baseGraphic.copy()[mask.minX:mask.maxX, mask.minY:mask.maxY]
 
         groupSettings = []
-        for cMask in self.classList:
+        for cMask in self.selectedImage.classList:
             if cMask.maskList.__contains__(mask):
                 groupSettings.append(cMask.maskSettings)
 
-        for gMask in self.groupList:
+        for gMask in self.selectedImage.groupList:
             if gMask.maskList.__contains__(mask):
                 groupSettings.append(gMask.maskSettings)
 
@@ -125,25 +153,25 @@ class MaskManager:
         drawingImg = self.simpleEdits.DrawBrightness(totalBrightness, drawingImg)
         drawingImg = self.simpleEdits.DrawSaturation(totalSaturation, drawingImg)
 
-        blankImg = self.baseImage.copy()
+        blankImg = self.baseGraphic.copy()
         blankImg[mask.minX:mask.maxX, mask.minY:mask.maxY] = drawingImg
-        mergedImg = np.where(mask.maskTrueFalse == True, blankImg, self.currentImage)
+        mergedImg = np.where(mask.maskTrueFalse == True, blankImg, self.selectedImage.currentGraphic)
 
-        cv2.imwrite(self.imagePath + 'p6_maskedit.png', mergedImg)
-        pixmap = QPixmap(self.imagePath + 'p6_maskedit.png')
-        self.displayGraphic.setPixmap(pixmap)
-        self.currentImage = mergedImg
+        cv2.imwrite(self.imageFullPath + self.editFilename, mergedImg)
+        pixmap = QPixmap(self.imageFullPath + self.editFilename)
+        self.displayGraphicLabel.setPixmap(pixmap)
+        self.selectedImage.currentGraphic = mergedImg
 
     # This version is slightly slower than the new one
     def DrawMaskOLD(self, mask):
-        drawingImg = self.baseImage.copy()
+        drawingImg = self.baseGraphic.copy()
 
         groupSettings = []
-        for cMask in self.classList:
+        for cMask in self.selectedImage.classList:
             if cMask.maskList.__contains__(mask):
                 groupSettings.append(cMask.maskSettings)
 
-        for gMask in self.groupList:
+        for gMask in self.selectedImage.groupList:
             if gMask.maskList.__contains__(mask):
                 groupSettings.append(gMask.maskSettings)
 
@@ -157,12 +185,12 @@ class MaskManager:
         drawingImg = self.simpleEdits.DrawBrightness(totalBrightness, drawingImg)
         drawingImg = self.simpleEdits.DrawSaturation(totalSaturation, drawingImg)
 
-        mergedImg = np.where(mask.maskTrueFalse == True, drawingImg, self.currentImage)
+        mergedImg = np.where(mask.maskTrueFalse == True, drawingImg, self.currentGraphic)
 
         cv2.imwrite('imageData/p6_maskedit.png', mergedImg)
         pixmap = QPixmap('imageData/p6_maskedit.png')
-        self.displayGraphic.setPixmap(pixmap)
-        self.currentImage = mergedImg
+        self.displayGraphicLabel.setPixmap(pixmap)
+        self.currentGraphic = mergedImg
 
     def DrawSelectedMask(self):
         if self.selectedIsGroup:
@@ -200,5 +228,5 @@ class MaskManager:
         print("update curvetool")
 
     def BlurChange(self, blurValue, backgroundState, filterState):
-        self.blur.BlurFilter(self.currentImage, self.selectedMask, self.imagePath, blurValue, backgroundState, filterState)
-        self.displayGraphic.setPixmap(QPixmap(self.imagePath + 'p6_maskedit.png'))
+        self.blur.BlurFilter(self.currentGraphic, self.selectedMask, self.imageFullPath, blurValue, backgroundState, filterState)
+        self.displayGraphicLabel.setPixmap(QPixmap(self.imageFullPath + self.editFilename))
